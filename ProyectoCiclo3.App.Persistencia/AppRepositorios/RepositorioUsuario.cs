96f07b27075761d456ef962b63b878993f9ae25a
AlexanderPRD <alexander2217@gmail.com>
2021-09-25 09:56:33 -0500

    cambios sabado

---
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/RepositorioUsuario.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using ProyectoCiclo3.App.Dominio;
+using System.Linq;
+using System;
+ 
+namespace ProyectoCiclo3.App.Persistencia.AppRepositorios
+{
+    public class RepositorioUsuario
+    {
+        List<Usuario> Usuario;
+ 
+    public RepositorioUsuario()
+        {
+            Usuario= new List<Usuario>()
+            {
+                new Usuario{id=1,nombre="Mario",apellido= "Sánchez",direccion= "Carrera 27",telefono= "8945412121"},
+                new Usuario{id=2,nombre="Camila",apellido= "Rodriguez",direccion= "Calle 96",telefono= "895656562"},
+                new Usuario{id=3,nombre="Maria Carolina",apellido= "Perez",direccion= "Avenida Sur con 14",telefono= "12145454"} 
+            };
+        }
+ 
+        public IEnumerable<Usuario> GetAll()
+        {
+            return Usuario;
+        }
+ 
+        public Usuario GetUsuarioWithId(int id){
+            return Usuario.SingleOrDefault(b => b.id == id);
+        }
+    }
+}
