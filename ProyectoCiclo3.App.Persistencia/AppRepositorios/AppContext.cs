eb35e998642649dbccd01f8cc6f5e2859458faa3
AlexanderPRD <alexander2217@gmail.com>
2021-10-19 12:00:38 -0500

    DespliegueProyecto

--- ProyectoCiclo3.App.Persistencia/AppRepositorios/AppContext.cs
+++ ProyectoCiclo3.App.Persistencia/AppRepositorios/AppContext.cs
@@ -9,8 +9,10 @@ namespace ProyectoCiclo3.App.Persistencia
         public DbSet<Usuario> Usuario { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder){
             if(!optionsBuilder.IsConfigured){
-                optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ProyectoCiclo03");
+                // optionsBuilder.UseSqlServer("Data Source = (localdb)\\MSSQLLocalDB; Initial Catalog = ProyectoCiclo03");
+                optionsBuilder.UseSqlServer("Data Source = misiontic2021equipo10.database.windows.net; Initial Catalog = ProyectoCiclo03; User ID=admin_equipo10_grupo27;Password=JQTpkLMWX6JBhMu");
             }
         }
     }
 }
+
